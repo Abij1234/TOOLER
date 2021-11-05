d3b75def214833152581fbb1e862a7b333f054cb
Abij1234 <abijithjr63@gmail.com>
2021-11-05 14:15:07 +0530

    repository initialised

---
+++ tooler.sh
@@ -0,0 +1,665 @@
+#!/usr/bin/env bash
+echo -e "
+▀▀█▀▀ ▒█▀▀▀█ ▒█▀▀▀█ ▒█░░░ ▒█▀▀▀ ▒█▀▀█ 
+░▒█░░ ▒█░░▒█ ▒█░░▒█ ▒█░░░ ▒█▀▀▀ ▒█▄▄▀ 
+░▒█░░ ▒█▄▄▄█ ▒█▄▄▄█ ▒█▄▄█ ▒█▄▄▄ ▒█░▒█" | pv -qL 500 | lolcat
+echo
+echo -e "▒▒▒▒▒▒▐███████▌
+▒▒▒▒▒▒▐░▀░▀░▀░▌
+▒▒▒▒▒▒▐▄▄▄▄▄▄▄▌
+▄▀▀▀█▒▐░▀▀▄▀▀░▌▒█▀▀▀▄
+▌▌▌▌▐▒▄▌░▄▄▄░▐▄▒▌▐▐▐▐" | pv -qL 100 | lolcat
+echo
+printf "\033[1;31mWARNING \033[0m\n"
+printf "\033[1;36mThese tool not promote illegal activitys.Only use for educational purposes \033[0m\n"
+echo
+printf "\033[1;34m1>for legal tools \033[0m\n"
+printf "\033[1;34m2>for illegal tools \033[0m\n"
+printf "\033[1;34m3>to exit \033[0m\n"
+echo
+while true; do
+printf "\033[1;33mENTER AN OPTION==> \033[0m"
+read opt
+if [[ $opt == '1' || $opt == '01' ]]; then
+echo
+printf "\033[1;34mTOOLS AVAILABLE \033[0m\n"
+echo
+printf "\033[1;36m(a) learn-bash \033[0m\n"
+printf "\033[1;36m(b) your_phone \033[0m\n"
+printf "\033[1;36m(c) calculator \033[0m\n"
+printf "\033[1;36m(d) hash_effect \033[0m\n"
+printf "\033[1;36m(e) BHUTUU_bot \033[0m\n"
+printf "\033[1;36m(f) c.practice \033[0m\n"
+printf "\033[1;36m(g) java.practice \033[0m\n"
+printf "\033[1;36m(h) termux-fashion \033[0m\n"
+printf "\033[1;36m(i) termux-backup \033[0m\n"
+printf "\033[1;36m(j) tg-bot \033[0m\n"
+printf "\033[1;36m(k) calandar \033[0m\n"
+printf "\033[1;36m(l) PACKAGES \033[0m\n"
+printf "\033[1;36m(m) dictionary \033[0m\n"
+printf "\033[1;36m(n) liveserver \033[0m\n"
+printf "\033[1;36m(o) ngrok \033[0m\n"
+echo
+printf "\033[1;34mSELECT A TOOL WANT TO INSTALL==> \033[0m"
+read tool
+if [[ $tool == 'a' || $tool == 'A' ]]; then
+echo
+printf "\033[1;31mdownlording. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+echo
+git clone https://github.com/Abij1234/learn-bash.git
+echo
+printf "\033[1;34mDOWNLORDED SUCESSFULLY \033[0m\n"
+echo
+elif [[ $tool == 'b' || $tool == 'B' ]]; then
+echo
+printf "\033[1;31mdownlording. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+echo
+git clone https://github.com/Abij1234/your_phone.git
+echo
+printf "\033[1;34mDOWNLORDED SUCESSFULLY \033[0m\n"
+echo
+elif [[ $tool == 'c' || $tool == 'C' ]]; then
+echo
+printf "\033[1;31mdownlording. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+echo
+git clone https://github.com/Abij1234/calculator.git
+echo
+printf "\033[1;34mDOWNLORDED SUCESSFULLY \033[0m\n"
+echo
+elif [[ $tool == 'd' || $tool == 'D' ]]; then
+echo
+printf "\033[1;31mdownlording. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+echo
+git clone https://github.com/Abij1234/hash_effect.git
+echo
+printf "\033[1;34mDOWNLORDED SUCESSFULLY \033[0m\n"
+echo
+elif [[ $tool == 'e' || $tool == 'E' ]]; then
+echo
+printf "\033[1;31mdownlording. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+echo
+git clone https://github.com/BHUTUU/BHUTUU_bot.git
+echo
+printf "\033[1;34mDOWNLORDED SUCESSFULLY \033[0m\n"
+echo
+elif [[ $tool == 'f' || $tool == 'F' ]]; then
+echo
+printf "\033[1;31mdownlording. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+echo
+git clone https://github.com/BHUTUU/c.practice.git
+echo
+elif [[ $tool == 'g' || $tool == 'G' ]]; then
+echo
+printf "\033[1;31mdownlording. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+echo
+git clone https://github.com/BHUTUU/java.practice.git
+echo
+elif [[ $tool == 'h' || $tool == 'H' ]]; then
+echo
+printf "\033[1;31mdownlording. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+echo
+git clone https://github.com/BHUTUU/termux-fashion.git
+echo
+elif [[ $tool == 'i' || $tool == 'I' ]]; then
+echo
+printf "\033[1;31mdownlording. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+echo
+git clone https://github.com/BHUTUU/termux-backup.git
+echo
+elif [[ $tool == 'j' || $tool == 'J' ]]; then
+echo
+printf "\033[1;31mdownlording. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+echo
+git clone https://github.com/BHUTUU/tg-bot.git
+echo
+elif [[ $tool == 'k' || $tool == 'K' ]]; then
+echo
+printf "\033[1;31mdownlording. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+echo
+git clone https://github.com/Abij1234/calandar.git
+echo
+elif [[ $tool == 'l' || $tool == 'L' ]]; then
+echo
+printf "\033[1;31mdownlording. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+echo
+git clone https://github.com/Abij1234/PACKAGES.git
+echo
+elif [[ $tool == 'm' || $tool == 'M' ]]; then
+echo
+printf "\033[1;31mdownlording. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+echo
+git clone https://github.com/BHUTUU/dictionary.git
+echo
+elif [[ $tool == 'n' || $tool == 'N' ]]; then
+echo
+printf "\033[1;31mdownlording. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+echo
+git clone https://github.com/BHUTUU/liveserver.git
+echo
+elif [[ $tool == 'o' || $tool == 'O' ]]; then
+echo
+printf "\033[1;31mdownlording. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+echo
+git clone https://github.com/BHUTUU/Ngrok.git
+echo
+else
+printf "\033[1;31mPLEASE SELECT A VALID OPTION \033[0m\n"
+echo
+fi
+break
+elif [[ $opt == '2' || $opt == '02' ]]; then
+echo
+printf "\033[1;31mYOU SELECTED ILLEGAL TOOLS \033[0m\n"
+echo
+printf "\033[1;31mWARNING \033[0m\n"
+printf "\033[1;34mUSE THESE TOOLS ONLY IN YOUR RISK, USE ONLY FOR EDUCATIONAL PURPOSES \033[0m\n"
+echo
+printf "\033[1;31m(10) phisher \033[0m\n"
+printf "\033[1;31m(11) CCTV-HYJACK \033[0m\n"
+printf "\033[1;31m(12) metasploit_automation \033[0m\n"
+printf "\033[1;31m(13) your_droid \033[0m\n"
+printf "\033[1;31m(14) instagram-bruteforce \033[0m\n"
+printf "\033[1;31m(15) Hash-bruter \033[0m\n"
+printf "\033[1;31m(16) webphish \033[0m\n"
+printf "\033[1;31m(17) cameraphish \033[0m\n"
+printf "\033[1;31m(18) bomb.sh \033[0m\n"
+echo
+printf "\033[1;34mENTER TOOL WANTED==> \033[0m"
+read name
+if [[ $name == '10' ]]; then
+echo
+printf "\033[1;31mdownlording. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+echo
+git clone https://github.com/Abij1234/phisher.git
+echo
+elif [[ $name == '11' ]]; then
+echo
+printf "\033[1;31mdownlording. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+echo
+git clone https://github.com/Abij1234/CCTV-HYJACK.git
+echo
+elif [[ $name == '12' ]]; then
+echo
+printf "\033[1;31mdownlording. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+echo
+git clone https://github.com/Abij1234/metasploit_automation.git
+echo
+elif [[ $name == '13' ]]; then
+echo
+printf "\033[1;31mdownlording. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+echo
+git clone https://github.com/Abij1234/your_droid.git
+echo
+elif [[ $name == '14' ]]; then
+echo
+printf "\033[1;31mdownlording. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+echo
+git clone https://github.com/BHUTUU/instagram-bruteforce.git
+echo
+elif [[ $name == '15' ]]; then
+echo
+printf "\033[1;31mdownlording. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+echo
+git clone https://github.com/BHUTUU/Hash-bruter.git
+echo
+elif [[ $name == '16' ]]; then
+echo
+printf "\033[1;31mdownlording. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+echo
+git clone https://github.com/BHUTUU/webphish.git
+echo
+elif [[ $name == '17' ]]; then
+echo
+printf "\033[1;31mdownlording. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+echo
+git clone https://github.com/BHUTUU/cameraphish.git
+echo
+elif [[ $nane == '18' ]]; then
+echo
+printf "\033[1;31mdownlording. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+sleep 1
+printf "\033[1;31m. \033[0m"
+echo
+git clone https://github.com/King-Nazim/darkhack.git
+elif [[ $opt == '3' || $opt == '03' ]]; then
+echo
+printf "\033[1;31mYOU ARE EXITED \033[0m\n"
+exit 1
+else
+printf "\033[1;34mPLEASE SELECT A VALID OPTION \033[0m\n"
+fi
+fi
+done
+
+
+
